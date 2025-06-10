a6432c1b6ec795d83482c28fb194d1f0e517b516
Raygun <raygun200@gmail.com>
2025-06-10 17:26:52 +0530

    logical Operators
    Logical AND
    Logical OR
    Logical Not

---
+++ logical.js
@@ -0,0 +1,15 @@
+//LOGICAL AND
+console.log(false && false);
+console.log(true && false);
+console.log(false && true);
+console.log(true && true);
+
+// LOGICAL NOT
+console.log(false || false);
+console.log(true || false);
+console.log(false || true);
+console.log(true || true);
+
+//LOGICAL NOT
+console.log(!false);
+console.log(!true);

760a9d60a2d83d211baa71600fc00be21015a88f
Raygun <raygun200@gmail.com>
2025-06-11 18:30:13 +0530

    minor updates

--- store-api/server.js
+++ store-api/server.js
@@ -1,96 +1,76 @@
+// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Product = require('./models/Product');
 
 const app = express();
-
 app.use(bodyParser.json());
 
-//MongoDB Connection
-mongoose.connect(
-    'mongodb://localhost:27017/store', 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).then(()=>
-    console.log("Connection to MongoDB is Successfull")
-).catch((err)=>
-    console.error(err)
-);
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/store', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Server is up and running');
-})
+});
 
-app.listen('3000', () =>
-    console.log('Server is up and running on PORT 3000')
-);
-
-//Create a Product - Insert a Product
+  // Create a product
 app.post('/products', async (req, res) => {
-    console.log(req);
-    try
-    {
+    try {
         const product = new Product(req.body);
         await product.save();
         res.status(201).send(product);
-    }
-    catch(err)
-    {
-        res.status(500).send(err);
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
-//Get All Products
-app.get('/products', async(req, res)=> {
+// Get all products
+app.get('/products', async (req, res) => {
     try {
         const products = await Product.find();
         res.send(products);
-    }
-    catch(err)
-    {
+    } catch (err) {
         res.status(500).send(err);
-    }    
+    }
 });
 
-//Get Product By Id
-app.get('/products/:id', async (req, res)=> {
-    try
-    {
+// Get single product by ID
+app.get('/products/:id', async (req, res) => {
+    try {
         const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).send('Not found');
         res.send(product);
-        if(!product){
-            res.status(404).send('Not Found');
-        }
-    }
-    catch(err)
-    {
+    } catch (err) {
         res.status(500).send(err);
     }
 });
 
-//Update/PUT product
-app.put('/products/:id', async (req, res)=> {
-    try
-    {
-        const porduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true});
-        res.send(porduct);
-    }
-    catch(err)
-    {
-        res.status(500).send(err);
+// Update product
+app.put('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!product) return res.status(404).send('Not found');
+        res.send(product);
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
-app.delete('/products/:id', async (req, res)=> {
-    try
-    {
+// Delete product
+app.delete('/products/:id', async (req, res) => {
+    try {
         const product = await Product.findByIdAndDelete(req.params.id);
-        res.send(product);
-    }
-    catch(err){
+        if (!product) return res.status(404).send('Not found');
+        res.send({ message: 'Deleted successfully' });
+    } catch (err) {
         res.status(500).send(err);
     }
 });
+
+// Start the server
+app.listen(3000, () => console.log('Server running on port 3000'));
